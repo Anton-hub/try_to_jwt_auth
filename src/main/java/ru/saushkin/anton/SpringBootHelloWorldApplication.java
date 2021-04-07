ab513436430ce6ed6eb6d2c0f9b4adacc5e801e0
Anton <saushkin_anton@mail.ru>
2021-04-07 12:41:55 +0300

    JWT авторизация через spring security

--- src/main/java/ru/saushkin/anton/SpringBootHelloWorldApplication.java
+++ src/main/java/ru/saushkin/anton/SpringBootHelloWorldApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class SpringBootHelloWorldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
 }
